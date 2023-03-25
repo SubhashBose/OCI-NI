e650acabe07505cda2f771f56f8d75bf4fbc8d42
Subhash Bose <online@subhashbose.com>
2023-03-24 23:52:10 -0400

    fix:  panic: chacha20: counter overflow

--- waste/cpu.go
+++ waste/cpu.go
@@ -26,22 +26,31 @@ func CPU(interval time.Duration, duration time.Duration, percent float64, CPUcou
 
 	runtime.GOMAXPROCS(CPUcount)
 	for {
+		fmt.Println("[CPU] Starting stress on", time.Now())
+
 		for i := 0; i < CPUcount; i++ {
 			go func() {
 				runtime.LockOSThread()
 				tend := time.Now().Add(duration)
+				lp_cnt:=0
 				for ok := true; ok; ok = tend.After(time.Now()) {
 					loop_st:= time.Now()
 					for i := 0; i < 1; i++ {
 						cipher.XORKeyStream(buffer, buffer)
+						loop_st++;
 					}
 					loop_dur:= time.Since(loop_st)
+					if loop_st>131000 {
+						newCipher, err := chacha20.NewUnauthenticatedCipher(buffer[:32], buffer[:24])
+						if err == nil {
+							cipher = newCipher
+							loop_st:=0
+						}
+					}
 					time.Sleep(loop_dur*time.Duration((100-percent)/percent) )
 				}
 			}()
 		}
-
-		fmt.Println("[CPU] Starting stress on", time.Now())
 
 		// try to construct a new cipher
 		newCipher, err := chacha20.NewUnauthenticatedCipher(buffer[:32], buffer[:24])
