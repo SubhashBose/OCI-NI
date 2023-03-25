54d1bbe42e786f424609a43055021b0df98587de
Subhash Bose <online@subhashbose.com>
2023-03-25 09:52:54 -0400

    Update cpu.go

--- waste/cpu.go
+++ waste/cpu.go
@@ -23,7 +23,6 @@ func CPU(interval time.Duration, duration time.Duration, percent float64, CPUcou
 	if err != nil {
 		panic(cipher)
 	}
-	XOR_cnt:=0
 
 	runtime.GOMAXPROCS(CPUcount)
 	for {
@@ -32,6 +31,7 @@ func CPU(interval time.Duration, duration time.Duration, percent float64, CPUcou
 		for i := 0; i < CPUcount; i++ {
 			go func() {
 				runtime.LockOSThread()
+				XOR_cnt:=0
 				tend := time.Now().Add(duration)
 				for ok := true; ok; ok = tend.After(time.Now()) {
 					loop_st:= time.Now()
@@ -40,7 +40,7 @@ func CPU(interval time.Duration, duration time.Duration, percent float64, CPUcou
 					}
 					XOR_cnt+=1;
 					loop_dur:= time.Since(loop_st)
-					if XOR_cnt>=4*MiB {
+					if XOR_cnt>=4*MiB/CPUcount {
 						newCipher, err := chacha20.NewUnauthenticatedCipher(buffer[:32], buffer[:24])
 						if err == nil {
 							cipher = newCipher
