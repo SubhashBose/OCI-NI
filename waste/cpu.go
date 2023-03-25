085ae35350f3c0d39905f9a0c4d796205ebe6a23
Subhash Bose <online@subhashbose.com>
2023-03-25 18:04:48 -0400

    Update cpu.go

--- waste/cpu.go
+++ waste/cpu.go
@@ -18,33 +18,35 @@ func CPU(interval time.Duration, duration time.Duration, percent float64, CPUcou
 	}
 	rand.Read(buffer)
 
-	// construct XChaCha20 stream cipher
-	cipher, err := chacha20.NewUnauthenticatedCipher(buffer[:32], buffer[:24])
-	if err != nil {
-		panic(cipher)
-	}
-
 	runtime.GOMAXPROCS(CPUcount)
 	for {
 		fmt.Println("[CPU] Starting stress on", time.Now())
 
+		// construct XChaCha20 stream cipher
+		cipher, err := chacha20.NewUnauthenticatedCipher(buffer[:32], buffer[:24])
+		if err != nil {
+			panic(cipher)
+		}
+		XOR_cnt:=0
+
 		for i := 0; i < CPUcount; i++ {
 			go func() {
 				runtime.LockOSThread()
-				XOR_cnt:=0
 				tend := time.Now().Add(duration)
 				for ok := true; ok; ok = tend.After(time.Now()) {
 					loop_st:= time.Now()
 					for i := 0; i < 1; i++ {
 						cipher.XORKeyStream(buffer, buffer)
+						XOR_cnt++;
 					}
-					XOR_cnt+=1;
 					loop_dur:= time.Since(loop_st)
-					if XOR_cnt>=4*MiB/32/CPUcount {
+					if XOR_cnt>=4*MiB/32 {
 						newCipher, err := chacha20.NewUnauthenticatedCipher(buffer[:32], buffer[:24])
+						fmt.Println("[CPU] Counter reached", time.Now())
 						if err == nil {
 							cipher = newCipher
 							XOR_cnt=0
+							fmt.Println("[CPU] Replacing new", time.Now())
 						}
 					}
 					time.Sleep(loop_dur*time.Duration((100-percent)/percent*1000)/time.Microsecond ) // percent part is rounded down to 1ns, so mult by 1000 then div by 1us
@@ -52,12 +54,12 @@ func CPU(interval time.Duration, duration time.Duration, percent float64, CPUcou
 			}()
 		}
 
-		// try to construct a new cipher
-		newCipher, err := chacha20.NewUnauthenticatedCipher(buffer[:32], buffer[:24])
-		if err == nil {
-			cipher = newCipher
-		}
-
 		time.Sleep(interval)
+
+		// try to construct a new cipher
+		//newCipher, err := chacha20.NewUnauthenticatedCipher(buffer[:32], buffer[:24])
+		//if err == nil {
+		//	cipher = newCipher
+		//}
 	}
 }
